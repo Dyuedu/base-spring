9656a8a732b1a6c6ba389478c248c7d820bfe33d
duy <hoangduy20407@gmail.com>
2026-03-12 19:01:18 +0700

    add parse token with each request

--- src/main/java/demo/configuration/JwtTokenProvider.java
+++ src/main/java/demo/configuration/JwtTokenProvider.java
@@ -6,8 +6,10 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -18,28 +20,26 @@ import java.util.Map;
 
 @Component
 public class JwtTokenProvider {
+    private final UserDetailsService userDetailsService;
 
     @Value("${app.jwt.secret}")
     private String jwtSecret;
     @Value("${app.jwt.expiration-ms}")
-    private long   jwtExpirationMs;
+    private long jwtExpirationMs;
+
+    public JwtTokenProvider(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     public String generateToken(Authentication auth) {
         Map<String, Object> claims = new HashMap<>();
         Account account = (Account) auth.getPrincipal();
         claims.put("role", account.getRole().getName());
-        return Jwts.builder()
-                .setSubject(auth.getName())
-                .addClaims(claims)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-                .compact();
+        return Jwts.builder().setSubject(auth.getName()).addClaims(claims).setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs)).signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
     }
 
     public String getUsernameFromToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build()
-                .parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token) {
@@ -50,6 +50,13 @@ public class JwtTokenProvider {
             System.out.println("JWT không hợp lệ: {}" + ex.getMessage());
             return false;
         }
+    }
+
+    public Authentication getAuthentication(String token) {
+        String username = getUsernameFromToken(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
     }
 
     private Key getSigningKey() {
--- src/main/java/demo/filter/JwtAuthenticationFilter.java
+++ src/main/java/demo/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,12 @@
 package demo.filter;
 
+import demo.configuration.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,11 +14,22 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String rawtoken = request.getHeader("Authorization");
-        String token = rawtoken.replace("Bearer ", "");
-
+        String rawToken = request.getHeader("Authorization");
+        if (rawToken != null && rawToken.startsWith("Bearer ")) {
+            String token = rawToken.substring(7);
+            if (jwtTokenProvider.validateToken(token)){
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
         filterChain.doFilter(request, response);
     }
 }
